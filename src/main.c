4372883bc81255af6d40ad1536c74dcae08e3263
cherrythecool <138817584+cherrythecool@users.noreply.github.com>
2026-08-31 00:47:53 -0400

    get a more usable interface going, might switch to libflac to test speed diff

--- src/main.c
+++ src/main.c
@@ -11,6 +11,24 @@
 #include "cherryaudio.h"
 #include "opusenc.h"
 
+const char* file_path_get_ext(const char* path) {
+    bool found_period = false;
+    size_t last_period = 0;
+
+    for (size_t index = 0; path[index] != '\0'; ++index) {
+        if (path[index] == '.') {
+            found_period = true;
+            last_period = index;
+        }
+    }
+
+    if (!found_period) {
+        return NULL;
+    }
+
+    return path + last_period + 1;
+}
+
 bool strs_eql_nocase(const char* s1, const char* s2) {
     size_t l1 = strlen(s1);
     size_t l2 = strlen(s2);
@@ -77,7 +95,7 @@ int main(void) {
 
     struct dirent* ep;
     while ((ep = readdir(dp)) != NULL) {
-        const char* extension = cherryaudio_str_pathext(ep->d_name);
+        const char* extension = file_path_get_ext(ep->d_name);
 
         // TODO: lowercase & uppercase extension support
         if (strs_eql_nocase(extension, "flac") || strs_eql_nocase(extension, "wav")) {
@@ -116,47 +134,67 @@ int main(void) {
         printf("converting '%s' to opus '%s'\n", song_path, conversion_path);
         printf("decoding original file\n");
 
-        const char* extension = cherryaudio_str_pathext(songs[i]);
+        const char* extension = file_path_get_ext(songs[i]);
         cherryaudio_file_format format = strs_eql_nocase(extension, "flac") ? CHERRYAUDIO_FILE_FORMAT_FLAC : CHERRYAUDIO_FILE_FORMAT_WAV_AIFF;
 
-        cherryaudio_file song = cherryaudio_decode_from_path(song_path, format, CHERRYAUDIO_PCM_FORMAT_F32);
-        bool song_loaded = song.pcm_data.frames_len > 0;
+        cherryaudio_stream stream = cherryaudio_stream_from_path(song_path, format);
+        cherryaudio_metadata stream_meta = stream.metadata;
+        bool song_loaded = stream_meta.total_sample_count > 0;
     
         if (song_loaded) {
-            printf("song loaded with %llu audio frames\n", song.pcm_data.frames_len);
+            printf("stream loaded with %llu audio frames\n", stream_meta.total_sample_count);
         } else {
-            fprintf(stderr, "failed to load song\n");
+            fprintf(stderr, "failed to open song stream at path '%s'\n", song_path);
             return 1;
         }
 
-        printf("converting to opus now\n");
-    
+        printf("setting up opus encoder\n");
         OggOpusComments* comments = ope_comments_create();
     
         int encoder_init_error;
-        OggOpusEnc* encoder = ope_encoder_create_file(conversion_path, comments, song.metadata.sample_rate, song.metadata.channels, song.metadata.channels > 8 ? 255 : song.metadata.channels > 2, &encoder_init_error);
+        OggOpusEnc* encoder = ope_encoder_create_file(conversion_path, comments, stream_meta.sample_rate, stream_meta.channels, stream_meta.channels > 8 ? 255 : stream_meta.channels > 2, &encoder_init_error);
         if (encoder_init_error != OPE_OK) {
             fprintf(stderr, "failed to initialize opus encoder with error %d\n", encoder_init_error);
             return 1;
         }
-    
-        int encode_res = ope_encoder_write_float(encoder, song.pcm_data.frames, song.metadata.total_sample_count);
-        if (encode_res != OPE_OK) {
-            fprintf(stderr, "failed to encode opus with error %d\n", encode_res);
-            return 1;
+
+        uint64_t decode_buffer_size = stream_meta.sample_rate;
+        float* decode_buffer = calloc(decode_buffer_size, sizeof(float));
+
+        // todo: make the 1s buffer once and keep decoding into it from there
+        uint64_t samples_done = 0;
+        while (samples_done < stream_meta.total_sample_count) {
+            cherryaudio_pcm pcm = cherryaudio_stream_decode_pcm(stream, CHERRYAUDIO_PCM_FORMAT_F32, decode_buffer_size, decode_buffer);
+            if (pcm.frames_len == 0) {
+                printf("\nreached eof early\n");
+                break;
+            }
+
+            samples_done += pcm.frames_len / stream_meta.channels;
+
+            int encode_res = ope_encoder_write_float(encoder, pcm.frames, pcm.frames_len / stream_meta.channels);
+            if (encode_res != OPE_OK) {
+                fprintf(stderr, "failed to encode opus with error %d\n", encode_res);
+                return 1;
+            }
+
+            printf("\r%s: %f%% encoded", conversion_file, ((float)samples_done / (float)stream_meta.total_sample_count) * 100.0f);
+            fflush(stdout);
         }
-
-        printf("freeing old pcm frames\n");
-        free(song.pcm_data.frames);
-
-        printf("draining and destroying encoder now\n");
-    
+        
+        printf("\ndraining and destroying opus encoder now\n");
+        
         ope_encoder_drain(encoder);
         
         ope_encoder_destroy(encoder);
         ope_comments_destroy(comments);
 
-        printf("finished conversion\n");
+        printf("freeing cherryaudio stream\n");
+        cherryaudio_stream_free(stream);
+
+        free(decode_buffer);
+
+        printf("finished conversion of '%s'\n", song_path);
     }
 
     printf("freeing bs\n");
